f99ba63c5c0fdc0f89d262815fecce5987d84edd
magiclizi <407202761@qq.com>
2017-04-06 20:37:12 +0800

    123

--- src/utils/request.js
+++ src/utils/request.js
@@ -49,8 +49,8 @@ export default function request(url, options,without) {
           var redirect_uri = encodeURI(uri);
           var appId = 'wx4188036aadb09af1';
           var newUri = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${redirect_uri}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
-          // window.location = newUri;
-          request(newUri,{},true);
+          window.location = newUri;
+          // request(newUri,{},true);
         }
         else{
           alert(data.message);
