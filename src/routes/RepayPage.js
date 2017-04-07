57f58970788d85ff7abc5488f689ad36346cc9ac
magiclizi <407202761@qq.com>
2017-04-07 10:36:19 +0800

    123

--- src/routes/RepayPage.js
+++ src/routes/RepayPage.js
@@ -10,6 +10,9 @@ class RepayPage extends React.Component {
 
   constructor() {
     super();
+    this['state'] = {
+      orderInfo:null
+    }
   }
 
   componentWillMount() {
@@ -21,31 +24,44 @@ class RepayPage extends React.Component {
     repay().then(result=>{
       if(result){
         var orderInfo = result['orderInfo'];
-        console.log(orderInfo);
-        console.log('调用支付,支付成功后，设置箱子的使用状态为 0');
+        this.setState({orderInfo:orderInfo});
       }
     })
   }
 
+  payRent(){
+    alert('调用支付');
+  }
+
   renderAction(){
-    if(this.props.curRentInfo){
+    if(this.state.orderInfo){
       return(
         <div className = {rentPageCss['bg']}
              style = {{backgroundImage:'url(http://rentservice.b0.upaiyun.com/repay.jpeg)'}}>
-          <span style = {{fontSize:25,color:'white',marginBottom:'18vh',width:'75vw',textAlign:'center'}}>
-            猪猪提醒，亲的柜号是{this.props.curRentInfo['chestLogicId']}_{this.props.curRentInfo['boxId']}号，记得将球放入的同时，把门关上哦。还有私人物品，千万别忘了呢。点击确认。
-          </span>
-          <div onClick={()=>{this.repay()}} className = {rentPageCss['ball']}/>
+          <div onClick={()=>{this.payRent()}} className = {rentPageCss['ball']}/>
         </div>
       )
     }
     else{
-      return(
-        <div className = {rentPageCss['bg']}
-             style = {{backgroundImage:'url(http://rentservice.b0.upaiyun.com/openRentingError.jpg)'}}>
-          <div onClick={()=>{this.closeWeb()}} className = {rentPageCss['ball']}/>
-        </div>
-      )
+      if(this.props.curRentInfo){
+        return(
+          <div className = {rentPageCss['bg']}
+               style = {{backgroundImage:'url(http://rentservice.b0.upaiyun.com/repay.jpeg)'}}>
+          <span style = {{fontSize:25,color:'white',marginBottom:'16vh',width:'75vw',textAlign:'center'}}>
+            猪猪提醒，亲的柜号是{this.props.curRentInfo['chestLogicId']}_{this.props.curRentInfo['boxId']}号，记得将球放入的同时，把门关上哦。还有私人物品，千万别忘了呢。点击确认。
+          </span>
+            <div onClick={()=>{this.repay()}} className = {rentPageCss['ball']}/>
+          </div>
+        )
+      }
+      else{
+        return(
+          <div className = {rentPageCss['bg']}
+               style = {{backgroundImage:'url(http://rentservice.b0.upaiyun.com/openRentingError.jpg)'}}>
+            <div onClick={()=>{this.closeWeb()}} className = {rentPageCss['ball']}/>
+          </div>
+        )
+      }
     }
   }
 
