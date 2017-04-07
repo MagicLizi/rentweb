f0a151568214d979190171c19d4ea7642e938ec3
magiclizi <407202761@qq.com>
2017-04-07 08:30:21 +0800

    123

--- src/routes/QRScanPage.js
+++ src/routes/QRScanPage.js
@@ -32,7 +32,7 @@ class QRScanPage extends React.Component {
       });
 
       wx.error(function(res){
-        // alert(JSON.stringify(res));
+        alert(JSON.stringify(res));
         // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
 
       });
