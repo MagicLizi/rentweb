55c11e6d775e7aec41c93865abd7ddba9e570d79
magiclizi <407202761@qq.com>
2017-04-07 08:27:28 +0800

    123

--- src/routes/QRScanPage.js
+++ src/routes/QRScanPage.js
@@ -66,7 +66,7 @@ class QRScanPage extends React.Component {
 
   openQRScan(){
     wx.scanQRCode({
-      needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+      needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
       scanType: ["qrCode"], // 可以指定扫二维码还是一维码，默认二者都有
       success: function (res) {
         var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
