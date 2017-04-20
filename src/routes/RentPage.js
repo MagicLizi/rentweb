c7d144db19fc74c25e0d779807eac0ebaa46826c
magiclizi <407202761@qq.com>
2017-04-20 11:57:50 +0800

    123

--- src/routes/RentPage.js
+++ src/routes/RentPage.js
@@ -77,30 +77,29 @@ class RentPage extends React.Component{
     });
   }
 
-  selectCurInfo(){
+  componentDidMount() {
     var curRentInfo = this['props'].curRentInfo;
     if(curRentInfo.orderId){
-      var userId = curRentInfo.userId;
-      var orderId = curRentInfo.orderId;
+      var c = confirm('您有未支付的订单，请先支付！');
+      if(c){
+        var userId = curRentInfo.userId;
+        var orderId = curRentInfo.orderId;
 
-      payrent().then(result=>{
-        if(result){
-          var info = {
-            orderId : orderId,
-            path : '/rent',
-            userId:userId
+        payrent().then(result=>{
+          if(result){
+            var info = {
+              orderId : orderId,
+              path : '/rent',
+              userId:userId
+            }
+            var uri = `http://rentapi.magiclizi.com/pay/payment?info=${JSON.stringify(info)}`;
+            var redirect_uri = encodeURI(uri);
+            var newUri = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4188036aadb09af1&redirect_uri='
+              + uri + '&response_type=code&scope=snsapi_base#wechat_redirect';
+            window.location = newUri;
           }
-          var uri = `http://rentapi.magiclizi.com/pay/payment?info=${JSON.stringify(info)}`;
-          var redirect_uri = encodeURI(uri);
-          var newUri = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4188036aadb09af1&redirect_uri='
-            + uri + '&response_type=code&scope=snsapi_base#wechat_redirect';
-          window.location = newUri;
-        }
-      })
-
-    }
-    else{
-      this.closeWeb()
+        })
+      }
     }
   }
 
@@ -113,7 +112,7 @@ class RentPage extends React.Component{
              style = {{backgroundImage:'url(http://rentservice.b0.upaiyun.com/rentinservice.jpg!w640)'}}>
           <span style = {{fontSize:30,color:'white',marginBottom:'28vh'}}>亲的柜号是{curRentInfo['boxId']}号
           </span>
-          <div onClick={()=>{this.selectCurInfo()}} className = {rentPageCss['ball']}/>
+          <div onClick={()=>{this.closeWeb()}} className = {rentPageCss['ball']}/>
         </div>
       )
     }
