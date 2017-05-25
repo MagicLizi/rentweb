9a8750c2c0cda75bbde85352726e9dd320ef9719
magiclizi <407202761@qq.com>
2017-05-25 15:14:11 +0800

    123

--- src/routes/QRScanPage.js
+++ src/routes/QRScanPage.js
@@ -22,8 +22,6 @@ class QRScanPage extends React.Component {
 
   componentWillMount() {
     setCurPath('/qrScan');
-    var self = this;
-    // alert(this.props.location.pathname);
     var path = this.props.location.pathname;
     var search = this.props.location.search;
 
@@ -38,10 +36,6 @@ class QRScanPage extends React.Component {
         // if(self.props.location.query.direction){
         //   self.openQRScan();
         // }
-        alert('123');
-        // this.openQRScan();
-        // alert('ready');
-        // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
       });
 
       wx.error(function(res){
@@ -83,7 +77,7 @@ class QRScanPage extends React.Component {
               if(res.resultStr){
                 boxPrice(res.resultStr).then(r=>{
                   var price = r['price'].split('_');
-                  self.setState({step:1});
+                  alert(price);
                 })
 
                 // dealQRResult(res.resultStr).then(result=>{
