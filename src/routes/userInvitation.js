b0c04e1121d7f3080f5de9184e4c8c3485100b3a
magiclizi <407202761@qq.com>
2017-06-12 15:33:12 +0800

    123

--- src/routes/userInvitation.js
+++ src/routes/userInvitation.js
@@ -27,7 +27,7 @@ class UserInvitation extends React.Component{
       <div className = {Style.container}>
         {this.state.qr?(
         <div className={Style['qrContainer']}>
-          <div className={Style.title}>分享邀请码给你的朋友，你们都能获得3元充值奖励</div>
+          <div className={Style.title}>分享邀请码给你的朋友，你们分别能获得0.5元和3元的余额奖励哦！</div>
           <QRCode value={this.state.qr} size = {256}/>
         </div>
         ):null}
