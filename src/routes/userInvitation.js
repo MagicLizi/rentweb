34b1e9ba8c5ba66608ea6f64aef197082e48b5ab
magiclizi <407202761@qq.com>
2017-06-12 12:08:43 +0800

    123

--- src/routes/userInvitation.js
+++ src/routes/userInvitation.js
@@ -4,17 +4,29 @@
 import React from 'react';
 import Style from './userInvitation.css';
 import {getUserInvitation} from '../services/user';
+var QRCode = require('qrcode.react');
 class UserInvitation extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      qr:null
+    }
+  }
 
   componentWillMount() {
     getUserInvitation().then(result=>{
-
+      this.setState({qr:result.qr})
     })
   }
 
   render(){
     return(
-      <div className = {Style.container}>邀请</div>
+      <div className = {Style.container}>
+        {this.state.qr?(
+          <QRCode value={this.state.qr} size = {256}/>
+        ):null}
+      </div>
     )
   }
 }
